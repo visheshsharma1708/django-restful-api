924041974acc49652685299508e7299cbaf0e289
Your Name <your.email@example.com>
2024-07-30 14:05:16 +0530

    first commit

---
+++ mytutorial/urls.py
@@ -0,0 +1,10 @@
+
+# mytutorial/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('snippets.urls')),
+]
